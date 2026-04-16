0cb8cd8fff48cb4f170b6ae72ed602f40bc7344c
manishchawda <manishpchawda@yahoo.co.in>
2026-04-16 19:34:13 +0530

    Added Layer 1 Python learning new scripts - 3

---
+++ learning/layer1_basics/26_functions.py
@@ -0,0 +1,34 @@
+#def greet():
+#    print("Hello from function")
+
+#greet()
+
+#def greet(name):
+#    print("Hello", name)
+
+#greet("Manish")
+
+#def add_numbers(a, b):
+#    return a + b
+
+#result = add_numbers(10, 5)
+
+#print(result)
+
+
+#def greet(name="Guest"):
+#    print("Hello", name)
+
+#greet()
+#greet("Manish")
+
+#def create_user(name, role):
+#    print("Name:", name)
+#    print("Role:", role)
+
+#create_user(role="Admin", name="Manish")
+
+
+square = lambda x: x * x
+
+print(square(5))
